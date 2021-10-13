7b6b67570bd53a87301cb9e66defa435381a8251
Clayton  J. Wilkerson <clay2000lbs@gmail.com>
2021-10-13 11:26:50 -0500

    done

--- challenge-find-the-range/findTheRange.js
+++ challenge-find-the-range/findTheRange.js
@@ -2,7 +2,23 @@
 let array = [28, 43, -12, 30, 4, 0, -36]
 
 // Write your solution below:
+let max = array[0];
+for (let i = 1; i < array.length; ++i) {
+  if (array[i] > max) {
+    max = array[i];
+  }
+}
 
+console.log(max);
 
-console.log(Math.min(...array));
-console.log(Math.max(...array));
+let min = array[0];
+for (let i = 1; i < array.length; ++i) {
+  if (array[i] < min) {
+    min = array[i];
+  }
+}
+
+console.log(min);
+
+//console.log(Math.min(...array));
+//console.log(Math.max(...array));
