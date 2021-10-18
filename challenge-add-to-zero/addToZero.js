949232eb7eda0648b8beed0648ca4d20c59d436d
Clayton  J. Wilkerson <clay2000lbs@gmail.com>
2021-10-18 10:06:00 -0500

    alomst complete

---
+++ challenge-add-to-zero/addToZero.js
@@ -0,0 +1,15 @@
+// Starting array
+let array = [28, 43, -12, 30, 4, 0, 12]
+
+// Write your solution below:
+
+for (let i = 0; i < array.length, i++) {
+    for (let z = 0; i < array.length, i++) {
+        if (i !== z) {
+            if (array[i] + array[z] == 0) {
+                
+            }
+            
+        }
+    }
+}
